d6d3da112e57ad61270e66b14b2dc84bc9ef3e60
snnbotchway <solomonbotchway7@gmail.com>
2022-11-13 16:39:39 +0000

    Add more fields to user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -28,9 +28,19 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
         }),
     )
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    readonly_fields = ['last_login', 'date_joined']
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
