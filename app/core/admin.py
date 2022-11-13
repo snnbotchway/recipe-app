7e9bf18f5a212900091f2ff182ccee0e2d226260
snnbotchway <solomonbotchway7@gmail.com>
2022-11-13 17:20:43 +0000

    Change admin page headers

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,10 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 
+admin.site.site_header = 'Recipe App Admin Panel'
+admin.site.index_title = 'Administration'
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Define admin model for custom User model with no email field."""
