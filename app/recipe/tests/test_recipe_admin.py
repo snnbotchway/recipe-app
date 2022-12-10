744f2cb95475874146ff415953a78260bc7f3ba1
snnbotchway <solomonbotchway7@gmail.com>
2022-12-10 19:39:02 +0000

    Write tag admin tests

--- app/recipe/tests/test_recipe_admin.py
+++ app/recipe/tests/test_recipe_admin.py
@@ -5,7 +5,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from recipe.models import Recipe
+from recipe.models import Recipe, Tag
 
 
 def create_recipe(user, **params):
@@ -21,7 +21,15 @@ def create_recipe(user, **params):
     return Recipe.objects.create(user=user, **defaults)
 
 
-class AdminSiteTests(TestCase):
+def create_tag(user, **params):
+    """Creates and returns new tag"""
+    defaults = {"name": "Sample Tag Name"}
+    defaults.update(params)
+    return Tag.objects.create(user=user, **defaults)
+
+
+class RecipeAdminSiteTests(TestCase):
+
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -55,6 +63,43 @@ class AdminSiteTests(TestCase):
 
     def test_add_recipe_page(self):
         url = reverse('admin:recipe_recipe_add')
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class TagAdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testPass123',
+            first_name='admin_first_name',
+            last_name='admin_last_name'
+        )
+        self.client.force_login(self.admin_user)
+        self.tag = create_tag(user=self.admin_user)
+
+    def test_tag_list_page(self):
+        url = reverse('admin:recipe_tag_changelist')
+
+        response = self.client.get(url)
+
+        self.assertContains(response, self.tag.name)
+        self.assertContains(response, self.tag.user)
+
+    def test_tag_detail_page(self):
+        url = reverse('admin:recipe_tag_change',
+                      args=[self.tag.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_tag_page(self):
+        url = reverse('admin:recipe_tag_add')
 
         response = self.client.post(url)
 
