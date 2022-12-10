64c6b4e43c1d7c2ef3349da9509194677f4bec18
snnbotchway <solomonbotchway7@gmail.com>
2022-12-10 19:41:05 +0000

    Configure tag admin

--- app/recipe/admin.py
+++ app/recipe/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Recipe, Tag
 
 
 @admin.register(Recipe)
@@ -12,4 +12,13 @@ class RecipeAdmin(admin.ModelAdmin):
     search_fields = ['title__icontains']
     list_display = ['title', 'user', 'id', 'price', 'time_minutes']
     list_editable = ['price']
+    list_select_related = ['user']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Tag model configuration for the admin site"""
+    autocomplete_fields = ['user']
+    search_fields = ['name__icontains']
+    list_display = ['name', 'user', 'id']
     list_select_related = ['user']
