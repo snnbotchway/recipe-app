71be1a05c3250bf9c30a65513476e18ef03b13e5
snnbotchway <solomonbotchway7@gmail.com>
2022-12-10 18:36:04 +0000

    Write tag model tests

--- app/recipe/tests/test_recipe_models.py
+++ app/recipe/tests/test_recipe_models.py
@@ -6,31 +6,47 @@ from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from recipe.models import Recipe
+from recipe.models import Recipe, Tag
 
 
 class ModelTests(TestCase):
     """Test models"""
 
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            email='user@example.com',
+            password='testPass123',
+        )
+
     def test_create_recipe_successful(self):
         """Test creating a recipe is successful."""
-        user = get_user_model().objects.create_user(email='test@example.com',
-                                                    password='testPass123')
         title = 'Sample recipe name'
         time_minutes = 5
         price = Decimal('23.5')
         description = 'Sample recipe description.'
 
         recipe = Recipe.objects.create(
-            user=user,
+            user=self.user,
             title=title,
             time_minutes=time_minutes,
             price=price,
             description=description)
 
         self.assertEqual(recipe.title, str(recipe))
-        self.assertEqual(recipe.user, user)
+        self.assertEqual(recipe.user, self.user)
         self.assertEqual(recipe.title, title)
         self.assertEqual(recipe.time_minutes, time_minutes)
         self.assertEqual(recipe.price, price)
         self.assertEqual(recipe.description, description)
+
+    def test_create_tag_successful(self):
+        """Test creating a tag is successful."""
+        name = 'Sample tag name'
+
+        tag = Tag.objects.create(
+            user=self.user,
+            name=name)
+
+        self.assertEqual(tag.name, str(tag))
+        self.assertEqual(tag.user, self.user)
+        self.assertEqual(tag.name, name)
