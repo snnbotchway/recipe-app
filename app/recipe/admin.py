52652f3f6a2f0b628910369bfbd69e1efdc78d31
snnbotchway <solomonbotchway7@gmail.com>
2022-12-12 01:11:11 +0000

    Configure recipe-tag relation in admin panel

--- app/recipe/admin.py
+++ app/recipe/admin.py
@@ -5,20 +5,29 @@ from django.contrib import admin
 from .models import Recipe, Tag
 
 
+class TagInline(admin.TabularInline):
+    """Inline class for recipe tags and tag recipes."""
+    model = Recipe.tags.through
+    autocomplete_fields = ['tag', 'recipe']
+    extra = 1
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Recipe model configuration for the admin site"""
     autocomplete_fields = ['user']
-    search_fields = ['title__icontains']
+    inlines = [TagInline]
     list_display = ['title', 'user', 'id', 'price', 'time_minutes']
-    list_editable = ['price']
+    list_editable = ['price', 'time_minutes']
     list_select_related = ['user']
+    search_fields = ['title__icontains']
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     """Tag model configuration for the admin site"""
     autocomplete_fields = ['user']
-    search_fields = ['name__icontains']
+    inlines = [TagInline]
     list_display = ['name', 'user', 'id']
     list_select_related = ['user']
+    search_fields = ['name__icontains']
