7053545caccdf9331a272a95126f818c24f81c91
snnbotchway <solomonbotchway7@gmail.com>
2022-12-30 10:55:33 +0000

    Fix allowed host error in development

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
-        os.environ.get("ALLOWED_HOSTS", "").split(","),
+        os.environ.get("ALLOWED_HOSTS", "*").split(","),
     )
 )
 
