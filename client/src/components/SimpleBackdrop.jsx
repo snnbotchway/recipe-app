1e749a85786d08d1787b1e467de0644b6f5d5485
snnbotchway <solomonbotchway7@gmail.com>
2023-01-09 10:13:33 +0000

    Add backdrop component

---
+++ client/src/components/SimpleBackdrop.jsx
@@ -0,0 +1,17 @@
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function SimpleBackdrop() {
+	return (
+		<div>
+			<Backdrop
+				sx={{
+					color: "#fff",
+					zIndex: (theme) => theme.zIndex.drawer + 1,
+				}}
+				open={true}>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+		</div>
+	);
+}
